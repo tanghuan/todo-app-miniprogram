2bbcf0447490b555cb5d796c85e129ed5333da5a
tanghuan <tanghuanyou@163.com>
2021-05-16 10:05:40 +0800

    eslint

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,14 @@
-import * as React from 'react';
+import React, { ReactElement } from 'react';
 import { View, Text, Image } from 'remax/wechat';
 import styles from './index.css';
 
-export default () => {
+export default (): ReactElement => {
   return (
     <View className={styles.app}>
       <View className={styles.header}>
         <Image
           src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
           className={styles.logo}
-          alt="logo"
         />
         <View className={styles.text}>
           编辑 <Text className={styles.path}>src/pages/index/index.js</Text>{' '}
